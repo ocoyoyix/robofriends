ca140e58b954f6fc535d19231ee486f5f96d15d4
ocoyoyix <orlandocoyoy17@gmail.com>
2020-06-12 22:47:31 -0500

    front end of robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundry from "./ErrorBoundry";
 import "./App.css";
 
 class App extends Component {
@@ -37,7 +38,9 @@ class App extends Component {
           <h1 className="f1">Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filteredRobots} />
+            <ErrorBoundry>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
